9440cdec2a31b2f87fe711ef8918613dbb95f445
ricardog18 <gomezr30@uabc.edu.mx>
2026-02-26 19:04:00 -0800

    testmulti

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,7 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_multi():
+    calc = Calculadora()
+    assert calc.multi(2, 3) == 6
